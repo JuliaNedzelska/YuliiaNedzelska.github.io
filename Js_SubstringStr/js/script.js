27121133d92353d7f6177139e361af6df505c57a
y_nedzelska <y.nedzelskaya@gmail.com>
2017-08-11 11:21:46 +0300

    substrInStr

--- Js_SubstringStr/js/script.js
+++ Js_SubstringStr/js/script.js
@@ -1,9 +1,48 @@
+main();
+
 function main() {
-	// getSubstringPosition('String', 'ing');
+	var subsringPosition = getSubstringPosition('blalala', 'lala');
 	testGetSubstringPosition();
 }
 
 function getSubstringPosition(string, substring) {
+	var position = null;
+	var subStrCounter = 0;
+	console.log('Func getSubstringPosition');
+	console.log('	string & substring', string, substring);
+
+	if (string.length != 0 && substring.length < string.length) {
+		for (var i = 0; i < string.length; i++) {
+			for (var j = 0; j < substring.length; j++) {
+				if (string[i] == substring[j] && position == null) {
+					position = i;
+				}
+				else if (string[i+j] == substring[j]
+						&& substring[j] != substring[substring.length-1]
+						&& position != null) {
+
+				}
+				else if (string[i+j] == substring[j] 
+						&& substring[j] == substring[substring.length-1]
+						&& position != null) {
+							console.log('position', position);
+							return position;
+				}
+				else {
+					if (position == null) {
+						break;
+					}
+					else if (position != null) {
+						position = null;
+						break;
+					}
+				}
+			}
+			// console.log('string[i+1]', string[i+1]);
+			// console.log('string[j]', string[j]);	
+		}
+	}
+	console.log('position', position);
 	return position;
 }
 
@@ -18,7 +57,7 @@ function testGetSubstringPosition() {
 			testValue = 1;
 		} else return testValue = -1;
 
-		testResult = getSubstringPosition('  String', ' St');
+		testResult = getSubstringPosition('  String', 'St');
 		if (testResult == 2) {
 			testValue = 1;
 		} else return testValue = -1;
@@ -28,20 +67,17 @@ function testGetSubstringPosition() {
 			testValue = 1;
 		} else return testValue = -1;
 
-		testResult = getSubstringPosition('anotherString', 'not');
+		testResult = getSubstringPosition('anotherString', 'nor');
 		if (testResult == null) {
 			testValue = 1;
 		} else return testValue = -1;
 
-		testResult = getSubstringPosition('    ', 'do');
+		testResult = getSubstringPosition('DDDDDD', 'VVVVVVVVVVV');
 		if (testResult == null) {
 			testValue = 1;
 		} else return testValue = -1;
 
-		testResult = getSubstringPosition('AB', 'CCCCC');
-		if (testResult == null) {
-			testValue = 1;
-		} else return testValue = -1;
 	}
+	console.log('TestVale', testValue);
 	return testValue;
 }

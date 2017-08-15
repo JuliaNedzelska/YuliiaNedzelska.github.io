ee7e6973b9cf2a8cac2804932a8c1515f4a13999
y_nedzelska <y.nedzelskaya@gmail.com>
2017-08-15 11:50:18 +0300

    snake

--- JS_Field_2/js/script.js
+++ JS_Field_2/js/script.js
@@ -8,14 +8,14 @@ function main() {
 //game field creating
 function createGameField() {
     console.log('Func createGameField');
-
     for (var i = 0; i < 7; i++) {
-        document.getElementById('game').innerHTML += '<div class="row"></div>';
+    	document.getElementById('game').innerHTML += '<div class="row"></div>';
         for (var j = 0; j < 7; j++) {
         	document.getElementsByClassName('row')[i].innerHTML += '<div class="cell"></div>';
         }
     }
-    console.log('	game', game);
+    console.log('	gameField', game);
+    setSnakeOnField();
     runGame();
 }
 
@@ -39,6 +39,24 @@ function runGame() {
 	}, 1000);
 }
 
+function setSnakeOnField() {
+	var row = document.getElementsByClassName('row');
+	var cell = document.getElementsByClassName('cell');
+	var snakeHead = game[5];
+	console.log('	snakeHead', snakeHead);
+	var snakeDirection = 0;
+}
+
+function goLeft(snakeHeadCoords, previousDirection) {
+	if (!goRight()) {
+
+	}
+	return snakeHeadCoords;
+}
+
+/*
+ this function stops the game by adding class to gameField
+*/
 function stopGame() {
 	console.log('Func stopGame');
 	game.classList.add('gameOver');

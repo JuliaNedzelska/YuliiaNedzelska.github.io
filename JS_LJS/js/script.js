a75e2d045d369ecbb0e759fc39963c0a6e9222de
yuliia <y.nedzelskaya@gmail.com>
2017-10-23 22:24:48 +0300

    FSM

--- JS_LJS/js/script.js
+++ JS_LJS/js/script.js
@@ -3,9 +3,30 @@ var SWEET_WATER = 3;
 
 var machina = {
 	countCoins: 0,
-	getCoins: function() {
+	currentState: 'InitialState',
+	addCoins: function() {
 		console.log('Func getCoins \n countCoins - ', this.countCoins);
+		console.log('	currentState - ', this.currentState);
 		this.countCoins += +prompt('Add coins', 0);
+	},
+	changeState: function() {
+		switch(this.countCoins) {
+			case 1: this.currentState = 'NeedCoins';
+				getCoins();
+				break;
+			case 2:
+			case 3: this.currentState = 'MakeChoise';
+				makeChoise();
+		}
+	},
+	goNext: function() {
+		console.log('Func goNext');
+		console.log('	currentState - ', this.currentState);
+		switch(this.currentState) {
+			case 'NeedCoins': getCoins();
+				break;
+			case 'MakeChoise': makeChoise();
+		}
 	},
 	giveMineralWater: function() {
 		console.log('Func giveMineralWater \n countCoins - ', this.countCoins);
@@ -16,53 +37,38 @@ var machina = {
 		console.log('Func giveSweetWater \n countCoins - ', this.countCoins);
 		alert('Take your sweet water!');
 		this.countCoins = this.countCoins - SWEET_WATER;
-	},
-	giveRest: function() {
-		console.log('Func giveRest \n countCoins - ', this.countCoins);
-		if(this.countCoins != 0) {
-			alert('Take your rest, ' + this.countCoins + ' coins!');
-		}
 	}
 };
 
-/** Entry point */
-function main() {
-	addCoins();
-	giveWater(makeChoise());
-	machina.giveRest();
-}
 
 /** Ask user for the coins */
-function addCoins() {
-	console.log('Func addCoins');
-	while (machina.countCoins < MINERAL_WATER) {
-		machina.getCoins();
-	}
+function getCoins() {
+	machina.addCoins();
+	machina.changeState();
+	machina.goNext();
 }
 
 /** Defines user choise based on 2 variants:
- *  @return {number} 1 - MineralWater choise or 2 - SweetWater choise
+ *  1 - MineralWater choise
+ *  2 - SweetWater choise
  */
 function makeChoise() {
+	console.log('Func makeChoise');
 	var choise = 0;
 	if(machina.countCoins == MINERAL_WATER) {
 		choise = prompt('Press 1 to choose Mineral water or add more coins to make another choise.', 0);
+		if(choise == 1) {
+			machina.giveMineralWater();
+		}
+		else getCoins();
 	}
-	else if(machina.countCoins >= SWEET_WATER ) {
+	else if(machina.countCoins == SWEET_WATER ) {
 		choise = prompt('Press 1 to choose Mineral water or Press 2 to choose Sweet water.', 0);
-	}
-	return choise;
-}
-
-/** Gives Mineral or Sweet water based on makeChoise function result
- *  @choiseResult {function} - 1 - MineralWater choise or 2 - SweetWater choise
- */
-function giveWater(choiseResult) {
-		switch(+choiseResult) {
-		case 1: machina.giveMineralWater();
-				break;
-		case 2: machina.giveSweetWater();
+		if(choise == 1) {
+			machina.giveMineralWater();
+		}
+		else machina.giveSweetWater();
 	}
 }
 
-main();
+getCoins();

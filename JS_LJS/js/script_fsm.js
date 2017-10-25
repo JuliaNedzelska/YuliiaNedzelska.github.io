0b829c00d717f8568960c70be2ee75b488e5d509
yuliia <y.nedzelskaya@gmail.com>
2017-10-25 20:45:10 +0300

    fsm - art

--- JS_LJS/js/script_fsm.js
+++ JS_LJS/js/script_fsm.js
@@ -1,74 +1,127 @@
-var MINERAL_WATER = 2;
-var SWEET_WATER = 3;
+var waterGlass = document.getElementById('glass');
+var mineralWaterTitle = document.getElementById('title-mineral');
+var sweetWaterTitle = document.getElementById('title-sweet');
 
 var machina = {
 	countCoins: 0,
 	currentState: 'InitialState',
-	addCoins: function() {
-		console.log('Func getCoins \n countCoins - ', this.countCoins);
-		console.log('	currentState - ', this.currentState);
-		this.countCoins += +prompt('Add coins', 0);
+	mineralWaterTitleState: 'disable',
+	sweetWaterTitleState: 'disable',
+	addCoins: function(count) {
+		console.log('Func GetCoins \n 	CountCoins - ', this.countCoins);
+		console.log('	CurrentState - ', this.currentState);
+		this.countCoins += count;
 	},
 	changeState: function() {
+		console.log('Func ChangeState \n 	CurrentState - ', this.currentState);
+		console.log('	CountCoins - ', this.countCoins);
 		switch(this.countCoins) {
 			case 1: this.currentState = 'NeedCoins';
-				getCoins();
 				break;
-			case 2:
-			case 3: this.currentState = 'MakeChoise';
-				makeChoise();
-		}
-	},
-	goNext: function() {
-		console.log('Func goNext');
-		console.log('	currentState - ', this.currentState);
-		switch(this.currentState) {
-			case 'NeedCoins': getCoins();
+			case 2: this.currentState = 'MineralWaterState';
+				console.log('	mineralWaterTitle - ', mineralWaterTitle.id);
+				this.mineralWaterTitleState == 'active';
+				switchLightMineralTitle();
 				break;
-			case 'MakeChoise': makeChoise();
+			case 3: this.currentState = 'SweetWaterState';
+				console.log('	mineralWaterTitle - ', mineralWaterTitle.id);
+				if(this.mineralWaterTitleState == 'disable') {
+					switchLightMineralTitle();
+					switchLightSweetTitle();
+				}
+				else switchLightSweetTitle();
 		}
-	},
-	giveMineralWater: function() {
-		console.log('Func giveMineralWater \n countCoins - ', this.countCoins);
-		alert('Take your mineral water!');
-		this.countCoins = this.countCoins - MINERAL_WATER;
-	},
-	giveSweetWater: function() {
-		console.log('Func giveSweetWater \n countCoins - ', this.countCoins);
-		alert('Take your sweet water!');
-		this.countCoins = this.countCoins - SWEET_WATER;
 	}
 };
 
-
-/** Ask user for the coins */
-function getCoins() {
-	machina.addCoins();
-	machina.changeState();
-	machina.goNext();
-}
-
-/** Defines user choise based on 2 variants:
- *  1 - MineralWater choise
- *  2 - SweetWater choise
+/**
+ * switches the state for mineral water title
+ * from disabled to active and back by changing id attribute
  */
-function makeChoise() {
-	console.log('Func makeChoise');
-	var choise = 0;
-	if(machina.countCoins == MINERAL_WATER) {
-		choise = prompt('Press 1 to choose Mineral water or add more coins to make another choise.', 0);
-		if(choise == 1) {
-			machina.giveMineralWater();
-		}
-		else getCoins();
+function switchLightMineralTitle() {
+	if(machina.mineralWaterTitleState == 'disable') {
+		machina.mineralWaterTitleState = 'active';
+		mineralWaterTitle.id = 'title-mineral-active';
+	} else {
+		mineralWaterTitle.id = 'title-mineral';
+		machina.mineralWaterTitleState = 'disable';
+		sweetWaterTitle.id = 'title-sweet';
+		machina.sweetWaterTitleState = 'disable';
 	}
-	else if(machina.countCoins == SWEET_WATER ) {
-		choise = prompt('Press 1 to choose Mineral water or Press 2 to choose Sweet water.', 0);
-		if(choise == 1) {
-			machina.giveMineralWater();
-		}
-		else machina.giveSweetWater();
-	}
+	console.log('Func switchLightMineralTitle \n 	State ', machina.mineralWaterTitleState);
 }
 
-getCoins();
+/**
+ * switches the state for sweet water title
+ * from disabled to active and back by changing id attribute
+ */
+function switchLightSweetTitle() {
+	if(machina.sweetWaterTitleState == 'disable') {
+		machina.sweetWaterTitleState = 'active';
+		sweetWaterTitle.id = 'title-sweet-active';
+	} else {
+		mineralWaterTitle.id = 'title-mineral';
+		machina.mineralWaterTitleState = 'disable';
+		sweetWaterTitle.id = 'title-sweet';
+		machina.sweetWaterTitleState = 'disable';
+	}
+	console.log('Func switchLightSweetTitle \n 	State ', machina.sweetWaterTitleState);
+}
+
+/**
+ * switches the state for sweet water title
+ * from disabled to active and back by changing id attribute
+ */
+function chooseSweetWater() {
+	var sweetWaterPushButton = document.getElementsByClassName('push-button-sweet')[0];
+	sweetWaterPushButton.addEventListener("click", function(event) {
+		console.log('Func SweetWaterPushButton \n 	Event: ', event.target.id);
+		if(machina.sweetWaterTitleState == 'active') {
+			waterGlass.id = 'glassSweetlWater';
+			switchLightSweetTitle();
+			machina.countCoins -=3;
+		}
+	});
+}
+
+function chooseMineralWater() {
+	var mineralWaterPushButton = document.getElementsByClassName('push-button-mineral')[0];
+	mineralWaterPushButton.addEventListener("click", function(event) {
+		console.log('Func MineralWaterPushButton \n 	Event: ', event.target.id);
+		if(machina.mineralWaterTitleState == 'active') {
+			waterGlass.id = 'glassMineralWater';
+			switchLightMineralTitle();
+			machina.countCoins -=2;
+		}
+	});
+}
+
+function onClickCoins() {
+	var coins = document.getElementsByClassName('coins')[0];
+
+	coins.addEventListener("click", function(event) {
+	console.log('Func Start \n 	Element Target: ', event.target.id);
+	switch(event.target.id) {
+		case 'coin-1': machina.addCoins(1);
+			break;
+		case 'coin-2': machina.addCoins(2);
+			break;
+		case 'coin-3': machina.addCoins(3);
+			break;
+		}
+		machina.changeState();
+	});
+}
+
+function pushButton() {
+	chooseSweetWater();
+	chooseMineralWater();	
+}
+
+/** Entry point */
+function start() {
+	onClickCoins();
+	pushButton();	
+}
+
+start();
